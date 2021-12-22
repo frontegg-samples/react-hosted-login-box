6d0fd4954bc955543c0f7dabc31753a50835bdd7
Aviad Mizrachi <aviadm@gmail.com>
2021-12-22 17:43:47 +0200

    useEffect for redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
-import { useAuth, useLoginWithRedirect } from "@frontegg/react";
+import { useEffect } from "react";
+import { useAuth, useLoginWithRedirect, AdminPortal } from "@frontegg/react";
+
 
 function App() {
   const { user, isAuthenticated } = useAuth();
   const loginWithRedirect = useLoginWithRedirect();
+
+  console.log('user - ', user);
+  console.log('isAuthenticated - ', isAuthenticated);
+  console.log('access-token - ', user?.accessToken);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
   return (
     <div className="App">
-      { isAuthenticated ? (
+      {isAuthenticated ? (
         <div>
           <div>
-            <img src={user?.profilePictureUrl} alt={user?.name}/>
+            <img src={user?.profilePictureUrl} alt={user?.name} />
           </div>
           <div>
             <span>Logged in as: {user?.name}</span>
           </div>
           <div>
             <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+          </div>
+          <div>
+          <button onClick={() => AdminPortal.show()}>Open admin portal</button>
           </div>
         </div>
       ) : (
