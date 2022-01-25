677ccf80d40f1a36245213d831c3d8f72c5a833b
Aviad Mizrachi <aviadm@gmail.com>
2022-01-25 15:52:03 +0200

    Updated package.json + logout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
 import { useEffect } from "react";
-import { useAuth, useLoginWithRedirect, AdminPortal } from "@frontegg/react";
+import {
+  useAuth,
+  useLoginWithRedirect,
+  AdminPortal,
+  useTenantsState
+} from "@frontegg/react";
+import {ContextHolder} from "@frontegg/rest-api";
 
 
 function App() {
   const { user, isAuthenticated } = useAuth();
+  const tenantsState = useTenantsState();
   const loginWithRedirect = useLoginWithRedirect();
 
   console.log('user - ', user);
   console.log('isAuthenticated - ', isAuthenticated);
-  console.log('access-token - ', user?.accessToken);
+  console.log('user - ', user);
+  console.log('tenants - ', tenantsState?.tenants);
 
   useEffect(() => {
     if (!isAuthenticated) {
+      console.log('user is not logged-on. going to loginWithRedirect')
       loginWithRedirect();
     }
   }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
 
   return (
     <div className="App">
@@ -31,7 +45,10 @@ function App() {
             <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
           </div>
           <div>
-          <button onClick={() => AdminPortal.show()}>Open admin portal</button>
+            <button onClick={() => AdminPortal.show()}>Open admin portal</button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
           </div>
         </div>
       ) : (
